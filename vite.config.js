bfa22c0c50ac31f46c3bfd9b96068695cd1ac200
mrsuarezg <cesar.bejucos.2014@gmail.com>
2023-12-21 17:37:51 -0600

    Integration of Inertia, JetStream with real-time refresh and production mode

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,62 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ command }) =>({
+    base: command === "serve" ? process.env.ASSET_URL || "" : `${process.env.ASSET_URL || ""}/dist/`,
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-});
+    server: {
+        fs: { allow: [`${process.cwd()}`] },
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+            port: 5173
+        }
+    },
+    build: {
+        manifest: true,
+        rollupOptions: {
+            input: 'resources/js/app.js',
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        const directories = id.split(path.sep);
+                        const moduleIndex = directories.findIndex(directory => directory === 'node_modules');
+                        const modulePath = directories.slice(moduleIndex + 1, directories.length).join(path.sep);
+                        const moduleName = modulePath.split(path.sep)[0];
+
+                        return `vendor/${moduleName}`;
+                    }
+                }
+            }
+        },
+        outDir: 'public/dist',
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+        },
+    },
+    optimizeDeps: {
+        // exclude: ['vuetify'],
+        entries: [
+            './resources/**/*.vue',
+        ],
+    },
+}));
